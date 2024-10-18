855984bcefad4c457c4af93edc0e05db95bfa422
NoaTheSoap <noa.sjoset@gmail.com>
2024-10-18 14:57:06 +0200

    Snowman

---
+++ Assets/!Scripts/Snowman_Scripts/Snowman_sc/SnowballRoll.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class SnowballRoll : MonoBehaviour
+{
+    private float mass = 0.5f;
+    
+    private Rigidbody rb;
+
+    public GameObject SnowRemover;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.linearVelocity.magnitude > 0.1f)
+        {
+            GrowSnowball();
+        }
+        if (mass > 1) 
+        {
+            gameObject.tag = "Interactable";
+        }
+
+    }
+
+    void GrowSnowball()
+    {
+        Debug.Log(rb.linearVelocity.magnitude);
+        float speed = (rb.linearVelocity.magnitude * Time.deltaTime)/30;
+        SetMass(speed);
+    }
+    
+
+    void SetMass(float value)
+    {
+        mass = Mathf.Clamp(value+mass, 0.4f, 2);
+        Vector3 vectorMass = new Vector3(mass, mass, mass);
+        transform.localScale = vectorMass;
+        SnowRemover.transform.localScale = vectorMass;
+        
+        float rbMass = Mathf.Clamp(mass, 1, 3);
+        rb.mass = rbMass;
+    }
+}
