d1c02db66952b923a3f77d2394270a19b99c2c82
MarzLars <lars_gustav@hotmail.com>
2024-10-19 12:42:14 +0200

    Import converted Vehicle Arcade Physics project. TODO: Fix shader problem in sample level.

---
+++ Assets/ArcadeVehiclePhysics/SampleLevel/OutlineShader/OutlineEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace ArcadeVehiclePhysics.SampleLevel.OutlineShader
+{
+    public class OutlineEffect : MonoBehaviour
+    {
+        Camera _camera;
+        public Material material;
+
+        public List<ShaderVariable> shaderVariables = new List<ShaderVariable>();
+
+        void Awake()
+        {
+            if (_camera != null)
+                return;
+            _camera = GetComponent<Camera>();
+            _camera.depthTextureMode = DepthTextureMode.DepthNormals;
+        }
+
+        // Postprocess the image
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            foreach (var shaderVariable in shaderVariables.Where(shaderVariable => shaderVariable.varName != ""))
+            {
+                material.SetFloat(shaderVariable.varName, shaderVariable.varValue);
+            }
+
+            Graphics.Blit(source, null as RenderTexture, material);
+        }
+
+        [System.Serializable]
+        public class ShaderVariable
+        {
+            public string varName;
+            public float varValue;
+        }
+    }
+}
---
+++ Assets/ArcadeVehiclePhysics/Vehicle/Scripts/CA - CarAbilities/CaAbility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace ArcadeVehiclePhysics.Vehicle.Scripts.CA___CarAbilities
+{
+    public abstract class CaAbility : MonoBehaviour
+    {
+        public string axisKey = "New Ability";
+        public KeyCode abilityButton;
+        public abstract void CheckInput();
+        public abstract void DoAbility();
+
+    }
+}
