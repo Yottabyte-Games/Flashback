dd70325d9a523342f5277c9af5b8a574d1e68684
BossmanCEO <hkalstad@gmail.com>
2024-11-06 17:03:13 +0100

    PUUUUUUUUUUUUUUUUUUUSH

---
+++ Assets/!Scripts/SceneTransitionTrigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEnterEvent : MonoBehaviour
+{
+    public UnityEvent onTriggerEnterEvent;
+
+    private void OnTriggerEnter(Collider
+ other)
+    {
+        onTriggerEnterEvent.Invoke();
+    }
+}
