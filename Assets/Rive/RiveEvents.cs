eb7870bb3d0708e1c50387fc91b4a37979d9d4bf
MarzLars <lars_gustav@hotmail.com>
2024-11-07 11:14:55 +0100

    Import Rive

---
+++ Assets/Rive/RiveEvents.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+//! An example implementation to access Rive Events.
+
+namespace Rive
+{
+    [RequireComponent(typeof(RiveScreen))]
+    public class RiveEvents : MonoBehaviour
+    {
+        RiveScreen _mRiveScreen;
+        void Start()
+        {
+            _mRiveScreen = GetComponent<RiveScreen>();
+            _mRiveScreen.OnRiveEvent += RiveScreen_OnRiveEvent;
+        }
+
+        void RiveScreen_OnRiveEvent(ReportedEvent reportedEvent)
+        {
+            Debug.Log($"Event received, name: \"{reportedEvent.Name}\", secondsDelay: {reportedEvent.SecondsDelay}");
+
+            // Access specific event properties
+            if (reportedEvent.Name.StartsWith("rating"))
+            {
+                var rating = reportedEvent["rating"];
+                var message = reportedEvent["message"];
+                Debug.Log($"Rating: {rating}");
+                Debug.Log($"Message: {message}");
+            }
+        }
+
+        void OnDisable()
+        {
+            _mRiveScreen.OnRiveEvent -= RiveScreen_OnRiveEvent;
+        }
+
+    }
+}
---
+++ Assets/Rive/RiveTexture.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+using Rive;
+
+public class RiveTexture : MonoBehaviour
+{
+    public Asset asset;
+    public Fit fit = Fit.contain;
+    public int size = 512;
+
+    RenderTexture _mRenderTexture;
+    Rive.RenderQueue _mRenderQueue;
+    Rive.Renderer _mRiveRenderer;
+    CommandBuffer _mCommandBuffer;
+
+    File _mFile;
+    Artboard _mArtboard;
+    StateMachine _mStateMachine;
+    public StateMachine stateMachine => _mStateMachine;
+
+    static bool FlipY()
+    {
+        switch (SystemInfo.graphicsDeviceType)
+        {
+            case GraphicsDeviceType.Metal:
+            case GraphicsDeviceType.Direct3D11:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    void Awake()
+    {
+        _mRenderTexture = new RenderTexture(TextureHelper.Descriptor(size, size));
+        _mRenderTexture.Create();
+
+        UnityEngine.Renderer renderer = GetComponent<UnityEngine.Renderer>();
+        Material material = renderer.material;
+        material.mainTexture = _mRenderTexture;
+
+        if (!FlipY())
+        {
+            // Flip the render texture vertically for OpenGL
+            material.mainTextureScale = new Vector2(1, -1);
+            material.mainTextureOffset = new Vector2(0, 1);
+        }
+
+        _mRenderQueue = new Rive.RenderQueue(_mRenderTexture);
+        _mRiveRenderer = _mRenderQueue.Renderer();
+        if (asset != null)
+        {
+            _mFile = File.Load(asset);
+            _mArtboard = _mFile.Artboard(0);
+            _mStateMachine = _mArtboard?.StateMachine();
+        }
+
+        if (_mArtboard != null && _mRenderTexture != null)
+        {
+            _mRiveRenderer.Align(fit, Alignment.Center, _mArtboard);
+            _mRiveRenderer.Draw(_mArtboard);
+
+            _mCommandBuffer = new CommandBuffer();
+            _mCommandBuffer.SetRenderTarget(_mRenderTexture);
+            _mCommandBuffer.ClearRenderTarget(true, true, UnityEngine.Color.clear, 0.0f);
+            _mRiveRenderer.AddToCommandBuffer(_mCommandBuffer);
+        }
+    }
+
+    void Update()
+    {
+        _mRiveRenderer.Submit();
+        GL.InvalidateState();
+        
+        if (_mStateMachine != null)
+        {
+            _mStateMachine.Advance(Time.deltaTime);
+        }
+    }
+}
