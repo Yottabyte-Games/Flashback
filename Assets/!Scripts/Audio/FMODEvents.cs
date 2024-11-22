1708e77e3f04a22187bb282ea2d48292b4c525e1
BossmanCEO <hkalstad@gmail.com>
2024-11-22 16:47:18 +0100

    SFX

--- Assets/!Scripts/Audio/FMODEvents.cs
+++ Assets/!Scripts/Audio/FMODEvents.cs
@@ -6,12 +6,17 @@ public class FMODEvents : MonoBehaviour
     [field: Header("Ambience")]
     [field: SerializeField] public EventReference AmbienceHubworld { get; private set; }
 
+
     [field: Header("Player SFX")]
     [field: SerializeField] public EventReference PlayerFootsteps { get; private set; }
 
 
-    [field: Header("Dialogue")]
+    [field: Header("Car SFX")]
+    [field: SerializeField] public EventReference Checkpoint { get; private set; }
+    [field: SerializeField] public EventReference Finishline { get; private set; }
 
+
+    [field: Header("Dialogue")]
     [field: SerializeField] public EventReference Dialogue { get; private set; }
 
     public static FMODEvents Instance { get; private set; }
