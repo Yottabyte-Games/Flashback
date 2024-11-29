050aeb2d7b9e9890e25d724857a686424d190e90
MarzLars <lars_gustav@hotmail.com>
2024-11-29 12:48:03 +0100

    PLAYER POSITION EDIT

--- Assets/!Scripts/PlayerPositionStorage.cs
+++ Assets/!Scripts/PlayerPositionStorage.cs
@@ -1,10 +1,9 @@
-using _Scripts.Generic;
 using UnityEngine;
+using _Scripts.Generic;
 namespace _Scripts
 {
     public class PlayerPositionStorage : Singleton<PlayerPositionStorage>
     {
         public Vector3 SavedPlayerPosition = Vector3.zero; // Default position if no save exists
-        public Quaternion SavedPlayerRotation; // Default rotation if no save exists
     }
 }
