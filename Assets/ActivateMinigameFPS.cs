26a60356bcf58efde584907f37e03499e45c0672
anatolmo <145755035+anatolmo@users.noreply.github.com>
2024-12-04 13:27:22 +0100

    Pushing scene HubWorldFPS

--- Assets/ActivateMinigameFPS.cs
+++ Assets/ActivateMinigameFPS.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
 public class ActivateMinigameFPS : MonoBehaviour
 {
     [SerializeField] GameObject catWeapon;
 
+
     void Start()
     {
         catWeapon.SetActive(false);
     }
+
+    void Update()
+    {
+        ActivateMinigame();
+    }
     
     public void ActivateMinigame()
     {
-        catWeapon.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.F)) 
+           catWeapon.SetActive(true);
     }
 }
--- Assets/YottabyteGames/FpsScripts/Spawning.cs
+++ Assets/YottabyteGames/FpsScripts/Spawning.cs
@@ -1,3 +1,4 @@
+using FMOD;
 using System.Collections;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
@@ -7,10 +8,14 @@ public class Spawning : MonoBehaviour
 {
     [SerializeField] private GameObject enemyAI;
     [SerializeField] private GameObject enemyAI2;
+    [SerializeField] private GameObject enemyAI3;
     [SerializeField] private GameObject boss = null;
 
-    [SerializeField] private GameObject enemySpawner;
-    [SerializeField] private GameObject enemySpawner2;
+    [SerializeField] private GameObject enemyMainSpawner;
+    [SerializeField] private GameObject enemyMainSpawner2;
+    [SerializeField] private GameObject enemyMainSpawner3;
+
+    public int nextSpawnerLocation = 0;
 
     [SerializeField] private float enemyInterval = 10f;
     [SerializeField] private float officeWorkerInterval = 15f;
@@ -18,22 +23,48 @@ public class Spawning : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(spawnEnemy(enemyInterval, enemyAI, enemySpawner, spawnerLifeValue));
-        //StartCoroutine(spawnEnemy(enemyInterval, enemyAI2, spawnerLifeValue+1));
+        StartCoroutine(spawnEnemy(enemyInterval, enemyAI, enemyMainSpawner, enemyMainSpawner2, spawnerLifeValue));
+        //StartCoroutine(spawnEnemy(enemyInterval, enemyAI2, enemySpawner2, spawnerLifeValue+1));
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy, GameObject enemySpawner, int spawnerLifeValue)
+    private IEnumerator spawnEnemy(float interval, GameObject enemy, GameObject enemySpawner, GameObject enemySpawner2, int spawnerLifeValue)
     {
         if (spawnerLifeValue == 0)
         {
+            enemySpawner.SetActive(false);
+            enemySpawner2.SetActive(true);
+            spawnerLifeValue = 5;
+            nextSpawnerLocation++;
+            StartNextCoroutine();
             yield break;
         }
         else
         {
-            yield return new WaitForSeconds(interval);
+            /*yield return new WaitForSeconds(interval);
             GameObject newEnemy = Instantiate(enemy, enemySpawner.transform.position, Quaternion.identity);
             spawnerLifeValue--;
-            StartCoroutine(spawnEnemy(interval, enemy, enemySpawner,spawnerLifeValue));
+            StartCoroutine(spawnEnemy(interval, enemy, enemySpawner,spawnerLifeValue));*/
+
+            do
+            {
+                yield return new WaitForSeconds(interval);
+                GameObject newEnemy = Instantiate(enemy, enemySpawner.transform.position, Quaternion.identity);
+                spawnerLifeValue--;
+            } while (spawnerLifeValue > 0);
+
+            StartCoroutine(spawnEnemy(interval, enemy, enemySpawner, enemySpawner2, spawnerLifeValue));
+        }
+    }
+
+    private void StartNextCoroutine()
+    {
+        if (nextSpawnerLocation == 1) 
+        {
+            StartCoroutine(spawnEnemy(enemyInterval, enemyAI2, enemyMainSpawner2, enemyMainSpawner3, spawnerLifeValue));
+        }
+        if (nextSpawnerLocation == 2)
+        {
+            StartCoroutine(spawnEnemy(enemyInterval, enemyAI3, enemyMainSpawner3, null, spawnerLifeValue));
         }
     }
 }
