59a75ebfe4a2d1d240a453d023d5183481945cf0
anatolmo <145755035+anatolmo@users.noreply.github.com>
2024-12-05 11:44:21 +0100

    Spawning adjustments. Times for enemy spawning

--- Assets/YottabyteGames/FpsScripts/Spawning.cs
+++ Assets/YottabyteGames/FpsScripts/Spawning.cs
@@ -16,48 +16,50 @@ public class Spawning : MonoBehaviour
     private GameObject enemyMainSpawner3;
     private GameObject enemyMainSpawner4;
 
-    public int nextSpawnerLocation = 0;
+    private int nextSpawnerLocation = 0;
+    private float firstEnemyInterval = 20f;
 
     [SerializeField] private float enemyInterval = 10f;
-    [SerializeField] private float officeWorkerInterval = 15f;
     [SerializeField] private int spawnerLifeValue = 5;
     [SerializeField] private bool bossSpawner = true;
 
     private void Start()
     {
         StartCoroutine(spawnEnemy(enemyInterval, enemyAI, enemyMainSpawner, enemyMainSpawner2, spawnerLifeValue, bossSpawner));
-        //StartCoroutine(spawnEnemy(enemyInterval, enemyAI2, enemySpawner2, spawnerLifeValue+1));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy, GameObject enemySpawner, GameObject enemySpawner2, int lifeValue, bool bossSpawner)
     {
+        UnityEngine.Debug.Log($"Spawner with interval: {interval}, remaining life: {spawnerLifeValue}");
+
         if (spawnerLifeValue == 0)
         {
             enemySpawner.SetActive(false);
             enemySpawner2.SetActive(true);
-            spawnerLifeValue = lifeValue;
             nextSpawnerLocation++;
+            spawnerLifeValue = lifeValue;
             StartNextCoroutine();
             yield break;
         }
         else
         {
-            /*yield return new WaitForSeconds(interval);
-            GameObject newEnemy = Instantiate(enemy, enemySpawner.transform.position, Quaternion.identity);
-            spawnerLifeValue--;
-            StartCoroutine(spawnEnemy(interval, enemy, enemySpawner,spawnerLifeValue));*/
-
             do
             {
-                print("relevant" + (bossSpawner));
-                print("relevant" + (spawnerLifeValue == 1) );
+                /*print("relevant" + (bossSpawner));
+                print("relevant" + (spawnerLifeValue == 1) );*/
                 if (bossSpawner == true && spawnerLifeValue == 1)
                 {
 
                     yield return new WaitForSeconds(interval);
                     GameObject newEnemy = Instantiate(boss, enemySpawner.transform.position, Quaternion.identity);
                     spawnerLifeValue--;
-                    //StartCoroutine(spawnEnemy(interval, enemy, enemySpawner, enemySpawner2, spawnerLifeValue, message));
+                }
+                else if (spawnerLifeValue == 6 || spawnerLifeValue == 5)
+                {
+                    yield return new WaitForSeconds(firstEnemyInterval);
+                    GameObject newEnemy = Instantiate(enemy, enemySpawner.transform.position, Quaternion.identity);
+                    spawnerLifeValue--;
+                    print("relevant" + spawnerLifeValue);
                 }
                 else
                 {
@@ -68,7 +70,7 @@ public class Spawning : MonoBehaviour
                 }
             } while (spawnerLifeValue > 0);
 
-            StartCoroutine(spawnEnemy(interval, enemy, enemySpawner, enemySpawner2, spawnerLifeValue, bossSpawner));
+            StartCoroutine(spawnEnemy(enemyInterval, enemy, enemySpawner, enemySpawner2, spawnerLifeValue, bossSpawner));
         }
     }
 
@@ -79,8 +81,8 @@ public class Spawning : MonoBehaviour
             StartCoroutine(spawnEnemy(enemyInterval, enemyAI2, enemyMainSpawner2, enemyMainSpawner3, 6, true));
         }
         if (nextSpawnerLocation == 2)
-        {
-            StartCoroutine(spawnEnemy(enemyInterval, enemyAI3, enemyMainSpawner3, enemyMainSpawner4, 5, true));
+        { 
+            StartCoroutine(spawnEnemy(enemyInterval, enemyAI3, enemyMainSpawner3, enemyMainSpawner4, 5, false));
         }
         if (nextSpawnerLocation == 3)
         {
